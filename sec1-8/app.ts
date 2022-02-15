464496c2ce69db730c8c5f50282999a62a2e18a0
tanakenDX <ktanaka0807@gmail.com>
2022-02-15 23:34:37 +0900

    20220215

--- sec1-8/app.ts
+++ sec1-8/app.ts
@@ -1,1 +1,9 @@
-console.log('ここにコードを書いていく!');
+function add(n1:number,n2:number){
+    return n1 + n2;
+}
+
+const number1 = '5';
+const number2 = 2.8;
+//jsでは+の演算子で足すとき、一つ目の変数が文字列なら文字列結合をしてしまう。
+const result = add(number1,number2);
+console.log(result);
