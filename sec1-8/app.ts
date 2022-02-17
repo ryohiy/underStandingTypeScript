2d9b252b2de7dd78a1661e779d1026aafdd2fbc7
tanakenDX <ktanaka0807@gmail.com>
2022-02-17 23:03:55 +0900

    20220217_JS_dynamicTyping

--- sec1-8/app.ts
+++ sec1-8/app.ts
@@ -1,15 +1,27 @@
-function add(n1:number,n2:number){
+function add(n1: number, n2: number, showRslt: boolean, phrase: string) {
     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
         throw new Error('入力値が正しくないよ')
     }
-    return n1 + n2;
+    const rslt = n1 + n2
+    if (showRslt) {
+        // console.log(phrase + n1 + n2);note 0217_1_これだと5+2.8が文字列連結で処理されてしまうので誤り。
+        // note 0217_2_次回は型推論について（なぜ変数には型を明示しなくてよいのか）
+        console.log(phrase + rslt);
+    }
+    return rslt;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+
+const printRslt = true;
+const rsltPhrase = 'Result: '
+
 //jsでは+の演算子で足すとき、一つ目の変数が文字列なら文字列結合をしてしまう。
-const result = add(number1,number2);
-console.log(result);
+// const result = add(number1,number2);
+// console.log(result);
+add(number1, number2, printRslt,rsltPhrase);
+
 
 //note 0216_1_jsでも型chk出来るけど、tsとどう違うのか？
 // JavaScriptには typeofという演算子がある
